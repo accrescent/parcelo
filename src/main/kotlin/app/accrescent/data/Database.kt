2e1280046b2088bb959423ae0457460d57145e72
lberrymage <logan@lberrymage.dev>
2023-03-10 14:10:35 -0700

    Remove redundant receiver parameter from configureDatabase()

--- src/main/kotlin/app/accrescent/data/Database.kt
+++ src/main/kotlin/app/accrescent/data/Database.kt
@@ -1,11 +1,10 @@
 package app.accrescent.data
 
-import io.ktor.server.application.Application
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun Application.configureDatabase() {
+fun configureDatabase() {
     Database.connect(
         "jdbc:h2:mem:parcelo;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false",
         "org.h2.Driver",
