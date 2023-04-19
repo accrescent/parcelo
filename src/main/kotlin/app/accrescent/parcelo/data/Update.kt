6a2ee3f82b96c050bc37437b6ea5e920a5ef01e5
lberrymage <logan@lberrymage.dev>
2023-04-18 23:12:21 -0700

    Improve database consistency of app updates
    A reviewer shouldn't be assigned to an update without a review issue
    group, and likewise, a review issue group shouldn't be assigned to an
    update without a reviewer. This is already enforced in the application
    logic, but for better security and robustness it's a good idea to
    enforce it at the database level as well.

--- src/main/kotlin/app/accrescent/parcelo/data/Update.kt
+++ src/main/kotlin/app/accrescent/parcelo/data/Update.kt
@@ -6,6 +6,8 @@ import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.or
 import java.util.UUID
 
 object Updates : UUIDTable("updates") {
@@ -17,6 +19,16 @@ object Updates : UUIDTable("updates") {
     val reviewIssueGroupId =
         reference("review_issue_group_id", ReviewIssueGroups, ReferenceOption.NO_ACTION).nullable()
     val submitted = bool("submitted").default(false)
+
+    init {
+        check {
+            // This is an XNOR over the nullness of reviewerId and reviewIssueGroupId. In other
+            // words, either both reviewerId and reviewIssueGroupId must be null or both must be
+            // non-null.
+            reviewerId.isNull().and(reviewIssueGroupId.isNull())
+                .or(reviewerId.isNotNull() and reviewIssueGroupId.isNotNull())
+        }
+    }
 }
 
 class Update(id: EntityID<UUID>) : UUIDEntity(id), ToSerializable<SerializableUpdate> {
