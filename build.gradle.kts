84fab1b03cf1088dec3e31578a4cd521fe9ac1cf
lberrymage <logan@lberrymage.dev>
2023-05-02 18:02:54 -0700

    Bump Protobuf Gradle plugin from 0.9.2 to 0.9.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
     id("com.github.node-gradle.node") version "4.0.0"
-    id("com.google.protobuf") version "0.9.2"
+    id("com.google.protobuf") version "0.9.3"
     id("io.ktor.plugin") version "2.3.0"
 }
 
