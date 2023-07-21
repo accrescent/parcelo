098de465478ff80d26eb08f10eda7c93af6d21c6
Alexander Capehart <alex@oxycblt.org>
2023-07-21 09:32:25 -0600

    Add 404 page

--- console/frontend/src/app/app-routing.module.ts
+++ console/frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './landing/landing.component';
 import { RegisterOkComponent } from './register-ok/register-ok.component';
 import { RegisterUnauthorizedComponent } from './register-unauthorized/register-unauthorized.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
     ] },
     { path: 'register/ok', component: RegisterOkComponent },
     { path: 'register/unauthorized', component: RegisterUnauthorizedComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
---
+++ console/frontend/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss'],
+  imports: [MatButtonModule],
+  standalone: true
+})
+export class PageNotFoundComponent {
+
+}
