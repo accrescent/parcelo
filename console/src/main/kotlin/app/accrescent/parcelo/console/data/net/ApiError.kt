3ad6804ce22c180bfa90936e0ded3f5c7a026473
Alexander Capehart <alex@oxycblt.org>
2023-07-31 15:47:25 -0600

    Update ApiError to new parser
    Update the set of possible ApiErrors to reflect the new errors returned
    when parsing APK sets. This also includes aggressively simplifying what
    ApiErrors are actually returned, given how many possible errors can be
    returned by the ApkSet parser now.

--- console/src/main/kotlin/app/accrescent/parcelo/console/data/net/ApiError.kt
+++ console/src/main/kotlin/app/accrescent/parcelo/console/data/net/ApiError.kt
@@ -13,134 +13,113 @@ class ApiError private constructor(
     val message: String,
 ) {
     companion object {
-        fun androidManifest(message: String) = ApiError(1, "Invalid Android manifest", message)
-        fun apkFormat(message: String) = ApiError(2, "APK parsing failed", message)
-        fun debugCertificate(message: String) = ApiError(3, "Debug certificate detected", message)
-        fun signatureVerification(message: String) =
-            ApiError(4, "Signature verification failed", message)
+        fun apkSetFormat(message: String) = ApiError(1, "Could not parse APK set", message)
 
-        fun signatureVersion(message: String) = ApiError(5, "Signature version(s) invalid", message)
-        fun zipFormat(message: String) = ApiError(6, "ZIP format error", message)
-        fun apksNotFound(message: String) = ApiError(7, "APKs not found", message)
-        fun baseApkNotFound(message: String) = ApiError(8, "Base APK not found", message)
-        fun baseApkTargetSdkNotFound(message: String) =
-            ApiError(9, "Base APK target SDK not found", message)
+        fun apkSetInvalid(message: String) = ApiError(2, "APK set is invalid", message)
 
-        fun baseApkVersionNameNotFound(message: String) =
-            ApiError(10, "Base APK version name not found", message)
+        fun apkSetUnsupported(message: String) = ApiError(3, "APK set unsupported", message)
 
-        fun bundletoolMetadata(message: String) =
-            ApiError(11, "Invalid bundletool metadata", message)
+        fun apkSetSignature(message: String) = ApiError(4, "APK set incorrectly signed", message)
 
-        fun bundletoolVersion(message: String) = ApiError(12, "Invalid bundletool version", message)
-        fun bundletoolVersionNotFound(message: String) =
-            ApiError(13, "Bundletool vesrion not found", message)
+        fun apkSetDebuggable(message: String) = ApiError(5, "APK set in debug mode", message)
 
-        fun debuggable(message: String) = ApiError(14, "Application is debuggable", message)
-        fun duplicateSplits(message: String) =
-            ApiError(15, "Duplicate split APKs detected", message)
+        fun apkSetTestable(message: String) = ApiError(6, "APK set in test mode", message)
 
-        fun invalidSplitName(message: String) =
-            ApiError(16, "Invalid split name encountered", message)
+        fun apkFormat(message: String) = ApiError(7, "Could not parse APK", message)
 
-        fun manifestInfoInconsistent(message: String) =
-            ApiError(17, "Inconsistent manifest info", message)
+        fun apkInvalid(message: String) = ApiError(8, "APK is invalid", message)
 
-        fun signingCertMismatch(message: String) =
-            ApiError(18, "Signing certificates don't match", message)
+        fun apkSignature(message: String) = ApiError(9, "APK incorrectly signed", message)
 
-        fun targetSdkNotFound(message: String) = ApiError(19, "Target SDK not found", message)
-        fun testOnly(message: String) = ApiError(20, "Application is test only", message)
-        fun versionNameNotFound(message: String) = ApiError(21, "Version name not found", message)
         fun iconImageFormat() =
-            ApiError(22, "Image is not in an acceptable format", "Icon must be a PNG")
+            ApiError(10, "Image is not in an acceptable format", "Icon must be a PNG")
 
         fun imageResolution() = ApiError(
-            23,
+            11,
             "Image is not in an acceptable resolution",
             "Image resolution must be 512 x 512",
         )
 
         fun labelLength() = ApiError(
-            24,
+            12,
             "Label has invalid length",
             "Label length must be between 3 and 30 characters long (inclusive)",
         )
 
         fun unknownPartName(partName: String?) =
-            ApiError(25, "Unknown part name encountered", "Unknown part name \"$partName\"")
+            ApiError(13, "Unknown part name encountered", "Unknown part name \"$partName\"")
 
         fun appAlreadyExists() =
-            ApiError(26, "App already exists", "An app with this ID already exists")
+            ApiError(14, "App already exists", "An app with this ID already exists")
 
         fun minTargetSdk(min: Int, actual: Int) = ApiError(
-            27,
+            15,
             "Target SDK is too small",
             "Target SDK $actual is smaller than the minimum $min",
         )
 
         fun minBundletoolVersion(min: String, actual: String) = ApiError(
-            28,
+            16,
             "Bundletool version is too small",
             "Bundletool version $actual is smaller than the minimum $min",
         )
 
         fun missingPartName() = ApiError(
-            29,
+            17,
             "Missing request part names",
             "An expected part or parts is missing from the request",
         )
 
         fun invalidUuid(id: String) = ApiError(
-            30,
+            18,
             "Invalid UUID",
             "The ID \"$id\" supplied in the request is not a valid UUID",
         )
 
         fun draftNotFound(id: UUID) =
-            ApiError(31, "Draft not found", "A draft with id \"$id\" does not exist")
+            ApiError(19, "Draft not found", "A draft with id \"$id\" does not exist")
 
         fun reviewerAlreadyAssigned() = ApiError(
-            32,
+            20,
             "Reviewer already assigned",
             "A reviewer has already been assigned to this object",
         )
 
         fun alreadyReviewed() =
-            ApiError(33, "Already reviewed", "This object has already been reviewed")
+            ApiError(21, "Already reviewed", "This object has already been reviewed")
 
         fun reviewForbidden() = ApiError(
-            34,
+            22,
             "Review forbidden",
             "This user does not have sufficient access rights to review this object",
         )
 
         fun publishForbidden() = ApiError(
-            35,
+            23,
             "Publish forbidden",
             "This user does not have sufficient access rights to publish this object",
         )
 
         fun updateCreationForbidden() = ApiError(
-            36,
+            24,
             "Update forbidden",
             "This user does not have sufficient permissions to create an update for this app",
         )
 
         fun updateNotFound(id: UUID) =
-            ApiError(37, "Update not found", "An update with id \"$id\" does not exist")
+            ApiError(25, "Update not found", "An update with id \"$id\" does not exist")
 
         fun appNotFound(id: String) =
-            ApiError(38, "App not found", "An app with id \"$id\" does not exist")
+            ApiError(26, "App not found", "An app with id \"$id\" does not exist")
 
         fun readForbidden() = ApiError(
-            39,
+            27,
             "Read forbidden",
             "This user does not have sufficient access rights to read this object",
         )
 
         fun updateVersionTooLow(updateVersion: Int, appVersion: Int) = ApiError(
-            40,
+            28,
             "Update version is too low",
             "Update version code \"$updateVersion\" is less than published app version \"$appVersion\"",
         )
@@ -149,44 +128,61 @@ class ApiError private constructor(
 
 fun toApiError(error: ParseApkResult.Error): ApiError = with(error) {
     when (this) {
-        ParseApkResult.Error.AndroidManifestError -> ApiError.androidManifest(message)
-        ParseApkResult.Error.ApkFormatError -> ApiError.apkFormat(message)
-        ParseApkResult.Error.DebugCertificateError -> ApiError.debugCertificate(message)
-        ParseApkResult.Error.SignatureVerificationError -> ApiError.signatureVerification(message)
-        ParseApkResult.Error.SignatureVersionError -> ApiError.signatureVersion(message)
-        ParseApkResult.Error.ZipFormatError -> ApiError.zipFormat(message)
+        ParseApkResult.Error.AndroidManifestError -> ApiError.apkInvalid(message)
+        ParseApkResult.Error.ApkFormatError -> ApiError.apkInvalid(message)
+        ParseApkResult.Error.DebugCertificateError -> ApiError.apkSignature(message)
+        ParseApkResult.Error.SignatureVerificationError -> ApiError.apkSignature(message)
+        ParseApkResult.Error.SignatureVersionError -> ApiError.apkSignature(message)
+        ParseApkResult.Error.ZipFormatError -> ApiError.apkFormat(message)
     }
 }
 
 fun toApiError(error: ParseApkSetResult.Error): ApiError = with(error) {
     when (this) {
+        is ParseApkSetResult.Error.MetadataParseError -> ApiError.apkSetFormat(message)
         is ParseApkSetResult.Error.ApkParseError -> toApiError(this.error)
-        ParseApkSetResult.Error.ApksNotFoundError -> ApiError.apksNotFound(message)
-        ParseApkSetResult.Error.BaseApkNotFoundError -> ApiError.baseApkNotFound(message)
-        ParseApkSetResult.Error.BaseApkTargetSdkUnspecifiedError -> ApiError.baseApkTargetSdkNotFound(
-            message
-        )
-
-        ParseApkSetResult.Error.BaseApkVersionNameUnspecifiedError -> ApiError.baseApkVersionNameNotFound(
-            message
-        )
-
-        ParseApkSetResult.Error.BundletoolMetadataError -> ApiError.bundletoolMetadata(message)
-        ParseApkSetResult.Error.BundletoolVersionError -> ApiError.bundletoolVersion(message)
-        ParseApkSetResult.Error.BundletoolVersionNotFoundError -> ApiError.bundletoolVersionNotFound(
-            message
-        )
-
-        ParseApkSetResult.Error.DebuggableError -> ApiError.debuggable(message)
-        ParseApkSetResult.Error.DuplicateSplitError -> ApiError.duplicateSplits(message)
-        is ParseApkSetResult.Error.InvalidSplitNameError -> ApiError.invalidSplitName(message)
-        ParseApkSetResult.Error.ManifestInfoInconsistentError -> ApiError.manifestInfoInconsistent(
-            message
-        )
-
-        ParseApkSetResult.Error.SigningCertMismatchError -> ApiError.signingCertMismatch(message)
-        ParseApkSetResult.Error.TargetSdkNotFoundError -> ApiError.targetSdkNotFound(message)
-        ParseApkSetResult.Error.TestOnlyError -> ApiError.testOnly(message)
-        ParseApkSetResult.Error.VersionNameNotFoundError -> ApiError.versionNameNotFound(message)
+        is ParseApkSetResult.Error.SigningCertMismatchError -> ApiError.apkSetSignature(message)
+        is ParseApkSetResult.Error.DebuggableError -> ApiError.apkSetDebuggable(message)
+        is ParseApkSetResult.Error.TestOnlyError -> ApiError.apkSetTestable(message)
+        is ParseApkSetResult.Error.ManifestInfoInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.ZipFormatError -> ApiError.apkSetFormat(message)
+        is ParseApkSetResult.Error.MetadataNotFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.ApksNotFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.BundletoolVersionError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.MetadataUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.VariantsNotFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.VariantTargetingUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.VariantTargetsNothingError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.VariantSDKUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.DuplicateVariantError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.ApkSetUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.ModuleUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.ModuleTargetingUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.ApkDescriptionUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.BaseApkDescriptionInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.DuplicateBaseSplitsError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.MultipleBaseApkDescriptionsError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.NoBaseSplitsFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.MultipleBaseSplitsFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.DuplicateSplitPathsError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.ApkTargetingUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.AbiUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.LangUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.DensityUnsupportedError -> ApiError.apkSetUnsupported(message)
+        is ParseApkSetResult.Error.ApkTargetsNothingError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.MultipleApkDescriptionsError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.SplitIdInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.NoSplitsFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.MultipleSplitsFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.BaseApkNotFoundError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.UnaccountedSplitsError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.AppIdInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.VersionCodeInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.VersionNameUnspecifiedError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.VersionNameInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.SdkUnspecifiedError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.SdkInconsistentError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.TargetSdkUnspecifiedError -> ApiError.apkSetInvalid(message)
+        is ParseApkSetResult.Error.ReviewIssueInconsistentError -> ApiError.apkSetInvalid(message)
     }
 }
