06733acb20dfc91a839cd490d181f029900506ba
Alexander Capehart <alex@oxycblt.org>
2023-08-03 20:21:23 -0600

    Improve responsiveness

--- console/frontend/src/app/app-list/app-list.component.ts
+++ console/frontend/src/app/app-list/app-list.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRippleModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 
 import { App } from '../app/app';
-import { AppCardComponent } from '../app-card/app-card.component';
 import { AppService } from '../app/app.service';
 
 @Component({
     selector: 'app-app-list',
     standalone: true,
-    imports: [AppCardComponent, CommonModule, MatGridListModule, MatRippleModule, MatCardModule],
+    imports: [CommonModule, MatRippleModule, MatCardModule],
     templateUrl: './app-list.component.html',
     styleUrls: ['./app-list.component.scss']
 })
