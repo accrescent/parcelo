e59378b0f4baa5f3aebe71f093f2ffa9d88046d0
Alexander Capehart <alex@oxycblt.org>
2023-09-04 20:31:22 -0600

    Partially merge draft editor w/new app form

--- console/frontend/src/app/app/draft.service.ts
+++ console/frontend/src/app/app/draft.service.ts
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { Observable, map, filter, catchError } from 'rxjs';
 
 import { Draft } from './draft';
 
@@ -16,22 +16,55 @@ export class DraftService {
 
     constructor(private http: HttpClient) {}
 
-    createDraft(apkSet: File, icon: File, label: string): Observable<HttpEvent<Draft>> {
-        const formData = new FormData();
-        formData.append('apk_set', apkSet);
-        formData.append('icon', icon);
-        formData.append('label', label);
-
-        const req = new HttpRequest('POST', this.draftsUrl, formData, { reportProgress: true });
-
-        return this.http.request(req);
-    }
-
     getDrafts(): Observable<Draft[]> {
         return this.http.get<Draft[]>(this.draftsUrl);
     }
 
+    createDraft(apkSet: File, icon: File, label: string): Observable<Draft | number> {
+        const formData = new FormData();
+        formData.append("apk_set", apkSet);
+        formData.append("icon", icon);
+        formData.append("label", label);
+        return this.http.post<Draft>(this.draftsUrl, formData, { observe: 'events', reportProgress: true }).pipe(
+            filter(event => event.type === HttpEventType.UploadProgress || event instanceof HttpResponse),
+            map(event => {
+                if (event.type === HttpEventType.UploadProgress) {
+                    return Math.round(100 * event.loaded / event.total!!);
+                } else if (event instanceof HttpResponse) {
+                    return event.body!!;
+                } else {
+                    throw new Error("unreachable");
+                }
+            }),
+            catchError(err => { 
+                if (!err.error) {
+                    // Unexpected non-ApiError, make up one 
+                    // TODO: Move this to interceptor
+                    err.error = {
+                        errorCode: -1,
+                        title: "Internal server error",
+                        message: "Please report the issue"
+                    };
+                }
+                throw err; 
+            })
+        );
+    }
+
+
     submitDraft(id: string): Observable<void> {
-        return this.http.patch<void>(`${this.draftsUrl}/${id}`, '');
+        return this.http.patch<void>(`${this.draftsUrl}/${id}`, '').pipe(
+            catchError(err => { 
+                if (!err.error) {
+                    // Unexpected non-ApiError, make up one 
+                    err.error = {
+                        errorCode: -1,
+                        title: "Internal server error",
+                        message: "Please report the issue"
+                    };
+                }
+                throw err; 
+            })
+        );
     }
 }
--- console/frontend/src/app/new-draft-screen/new-draft-screen.component.ts
+++ console/frontend/src/app/new-draft-screen/new-draft-screen.component.ts
@@ -3,9 +3,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { Component } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+import { FormContainerModule } from '../form-container/form-container.module';
 import { DraftService } from '../app/draft.service';
 import { NewDraftEditorComponent } from '../new-draft-editor/new-draft-editor.component';
 import { NewDraftForm } from '../new-draft-editor/new-draft-form';
@@ -13,7 +13,7 @@ import { NewDraftForm } from '../new-draft-editor/new-draft-form';
 @Component({
     selector: 'app-new-draft-screen',
     standalone: true,
-    imports: [NewDraftEditorComponent],
+    imports: [FormContainerModule, NewDraftEditorComponent],
     templateUrl: './new-draft-screen.component.html',
 })
 export class NewDraftScreenComponent {
@@ -21,7 +21,9 @@ export class NewDraftScreenComponent {
 
     submitNewDraft(form: NewDraftForm): void {
         this.draftService.createDraft(form.apkSet, form.icon, form.label).subscribe(event => {
-            if (event instanceof HttpResponse) {
+            if (typeof event === "number") {
+                // TODO
+            } else {
                 this.router.navigate(['apps']);
             }
         });
