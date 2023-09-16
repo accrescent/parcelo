f1ce76aa83c20c604b5a64029ffa027561027717
lberrymage <logan@lberrymage.dev>
2023-09-15 18:18:33 -0700

    Add draft upload progress bar

--- console/frontend/src/app/new-draft-screen/new-draft-screen.component.ts
+++ console/frontend/src/app/new-draft-screen/new-draft-screen.component.ts
@@ -3,7 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { Component } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Router } from '@angular/router';
 
 import { DraftService } from '../draft.service';
@@ -13,15 +14,19 @@ import { NewDraftForm } from '../new-draft-form';
 @Component({
     selector: 'app-new-draft-screen',
     standalone: true,
-    imports: [NewDraftEditorComponent],
+    imports: [MatProgressBarModule, NewDraftEditorComponent],
     templateUrl: './new-draft-screen.component.html',
 })
 export class NewDraftScreenComponent {
+    uploadProgress = 0;
+
     constructor(private draftService: DraftService, private router: Router) {}
 
     submitNewDraft(form: NewDraftForm): void {
         this.draftService.createDraft(form.apkSet, form.icon, form.label).subscribe(event => {
-            if (event instanceof HttpResponse) {
+            if (event.type === HttpEventType.UploadProgress) {
+                this.uploadProgress = 100 * event.loaded / event.total!;
+            } else if (event instanceof HttpResponse) {
                 this.router.navigate(['apps']);
             }
         });
