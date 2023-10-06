69c268ae145ba33292f1cd89e507d1e5cdb78265
lberrymage <logan@lberrymage.dev>
2023-10-06 13:48:50 -0700

    Bump Ktor from 2.3.4 to 2.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("plugin.serialization") version "1.9.10" apply false
     id("com.github.node-gradle.node") version "7.0.1" apply false
     id("com.google.protobuf") version "0.9.4" apply false
-    id("io.ktor.plugin") version "2.3.4" apply false
+    id("io.ktor.plugin") version "2.3.5" apply false
 }
 
 tasks.register("ci") {
