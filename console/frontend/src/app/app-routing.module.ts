212708a8f04bd0833754d1256ccc984a2e1d822a
lberrymage <logan@lberrymage.dev>
2023-10-11 16:02:51 -0700

    Lazily load every route destination
    This change reduces our raw initial bundle size by ~47%, decreasing
    initial loading times.

--- console/frontend/src/app/app-routing.module.ts
+++ console/frontend/src/app/app-routing.module.ts
@@ -5,29 +5,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppsScreenComponent } from './apps-screen/apps-screen.component';
 import { authGuard } from './auth.guard';
-import { ConsoleLayoutComponent } from './console-layout/console-layout.component';
-import { LoginComponent } from './login/login.component';
-import { LoginScreenComponent } from './login-screen/login-screen.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { NewDraftScreenComponent } from './new-draft-screen/new-draft-screen.component';
-import { UpdatesScreenComponent } from './updates-screen/updates-screen.component';
 
 const routes: Routes = [
-    { path: '', component: ConsoleLayoutComponent, canActivate: [authGuard], children: [
-        { path: '', redirectTo: 'apps', pathMatch: 'full' },
-        { path: 'apps', component: AppsScreenComponent },
-        { path: 'apps/:id/updates', component: UpdatesScreenComponent },
-        { path: 'drafts/new', component: NewDraftScreenComponent },
-        {
-            path: 'review',
-            loadChildren: () => import('./review/review.routes').then(m => m.REVIEW_ROUTES),
-        },
-    ] },
-    { path: 'login', component: LoginScreenComponent },
-    { path: 'auth/github/callback', component: LoginComponent },
-    { path: '**', component: PageNotFoundComponent },
+    {
+        path: '',
+        loadComponent: () =>
+            import('./console-layout/console-layout.component').then(m => m.ConsoleLayoutComponent),
+        canActivate: [authGuard],
+        children: [
+            { path: '', redirectTo: 'apps', pathMatch: 'full' },
+            {
+                path: 'apps',
+                loadComponent: () =>
+                    import('./apps-screen/apps-screen.component').then(m => m.AppsScreenComponent),
+            },
+            {
+                path: 'apps/:id/updates',
+                loadComponent: () => import('./updates-screen/updates-screen.component')
+                    .then(m => m.UpdatesScreenComponent),
+            },
+            {
+                path: 'drafts/new',
+                loadComponent: () => import('./new-draft-screen/new-draft-screen.component')
+                    .then(m => m.NewDraftScreenComponent),
+            },
+            {
+                path: 'review',
+                loadChildren: () => import('./review/review.routes').then(m => m.REVIEW_ROUTES),
+            },
+        ],
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./login-screen/login-screen.component').then(m => m.LoginScreenComponent),
+    },
+    {
+        path: 'auth/github/callback',
+        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: '**',
+        loadComponent: () =>
+            import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent),
+    },
 ];
 
 @NgModule({
