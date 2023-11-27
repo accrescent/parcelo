40ae48b5d536ca5869e56a7c2d180b6c67616c4a
lberrymage <logan@lberrymage.dev>
2023-11-27 15:55:58 -0700

    Grant editMetadata permission to all existing users on migrate
    All users are the sole administrators of their apps, so they ought to be
    granted the editMetadata permission when it's added.

--- console/src/main/resources/db/migration/V2__Add_edit_metadata_access_control.sql
+++ console/src/main/resources/db/migration/V2__Add_edit_metadata_access_control.sql
@@ -3,3 +3,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 
 ALTER TABLE access_control_lists ADD edit_metadata BOOLEAN DEFAULT 0 NOT NULL;
+
+-- It's okay to grant the edit_metadata permission to all users since we don't support multi-user
+-- app management yet.
+UPDATE access_control_lists SET edit_metadata = TRUE;
