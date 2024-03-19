89a094b1f2fe7f6f71464e7d19011dd99bf6f78b
Logan Magee <logan@lberrymage.dev>
2024-03-19 13:06:35 -0700

    Bump Kotlin from 1.9.22 to 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 plugins {
-    kotlin("jvm") version "1.9.22" apply false
-    kotlin("plugin.serialization") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.23" apply false
+    kotlin("plugin.serialization") version "1.9.23" apply false
     id("com.google.protobuf") version "0.9.4" apply false
     id("io.ktor.plugin") version "2.3.9" apply false
     id("org.jetbrains.dokka") version "1.9.20" apply false
