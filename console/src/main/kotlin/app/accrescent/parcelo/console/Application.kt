339850c65a09a489ba4360ed2e1a4fe523860d5c
lberrymage <logan@lberrymage.dev>
2024-07-03 22:01:39 -0700

    Bump version to 0.6.1

--- console/src/main/kotlin/app/accrescent/parcelo/console/Application.kt
+++ console/src/main/kotlin/app/accrescent/parcelo/console/Application.kt
@@ -38,7 +38,7 @@ fun main(args: Array<String>) = EngineMain.main(args)
 
 @OptIn(ExperimentalSerializationApi::class)
 fun Application.module() {
-    log.info("Starting Parcelo console 0.6.0")
+    log.info("Starting Parcelo console 0.6.1")
 
     val config = if (environment.developmentMode) {
         val fileStorageDir = System.getenv("FILE_STORAGE_BASE_DIR")
