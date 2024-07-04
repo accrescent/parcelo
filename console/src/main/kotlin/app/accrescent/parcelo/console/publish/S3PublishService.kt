accrescent/parcelo

commit bfac8551ba2bcb1f761a269c5a67f497e4cc4266

Fix incorrect publishing of x86_64 ABI splits
Because the world is cruel, Android's supported ABIs as reported by the
Android platform are as follows:

- armeabi-v7a
- arm64-v8a
- x86
- x86_64

Notably, the split names of each of these ABIs as reported by their
corresponding split APKs are "armeabi_v7a", "arm64_v8a", "x86", and
"x86_64". However, their names as reported by the Android platform use
hyphens instead of underscores... except for x86_64. Thus, we thought we
could be clever and replace the underscores with hyphens to match the
split names with the values on the Android platform, unknowingly
breaking ABI split publishing _only for x86_64_; and woe is me, trying
to test downloads on an x86_64 emulator.

For the record, I think this is at least the third time I've fixed
hypen/underscore confusion breaking downloads.

console/src/main/kotlin/app/accrescent/parcelo/console/publish/S3PublishService.kt
@@ -135,7 +135,12 @@ class S3PublishService(
                     val fileName = if (splitName.isEmpty) {
                         "base.apk"
                     } else {
-                        "split.${splitName.get().replace('_', '-')}.apk"
+                        val splitName = splitName.get().apply {
+                            if (this == "armeabi_v7a" || this == "arm64_v8a") {
+                                this.replace('_', '-')
+                            }
+                        }
+                        "split.$splitName.apk"
                     }
 
                     val request = PutObjectRequest {
@@ -170,7 +175,13 @@ class S3PublishService(
             val repoData = RepoData(
                 version = metadata.versionName,
                 versionCode = metadata.versionCode,
-                abiSplits = metadata.abiSplits.map { it.replace("_", "-") }.toSet(),
+                abiSplits = metadata.abiSplits.map {
+                    if (it == "armeabi_v7a" || it == "arm64_v8a") {
+                        it.replace('_', '-')
+                    } else {
+                        it
+                    }
+                }.toSet(),
                 langSplits = metadata.langSplits,
                 densitySplits = metadata.densitySplits,
                 shortDescription = shortDescription,

commit 1f02768b18e494fddf6553fff123671fa9c79095

Fix incorrect publishing of ARM splits
Ironically, this was caused by a bug introduced in bfac855: Fix
incorrect publishing of x86_64 ABI splits.

console/src/main/kotlin/app/accrescent/parcelo/console/publish/S3PublishService.kt
@@ -135,9 +135,11 @@ class S3PublishService(
                     val fileName = if (splitName.isEmpty) {
                         "base.apk"
                     } else {
-                        val splitName = splitName.get().apply {
+                        val splitName = splitName.get().run {
                             if (this == "armeabi_v7a" || this == "arm64_v8a") {
                                 this.replace('_', '-')
+                            } else {
+                                this
                             }
                         }
                         "split.$splitName.apk"