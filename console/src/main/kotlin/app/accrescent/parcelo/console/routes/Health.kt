100e45e8c3db3f44659ef9e8ef49b2c21a849a08
lberrymage <logan@lberrymage.dev>
2024-10-22 16:39:36 -0700

    Add basic health check route

---
+++ console/src/main/kotlin/app/accrescent/parcelo/console/routes/Health.kt
@@ -0,0 +1,25 @@
+// Copyright 2024 Logan Magee
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package app.accrescent.parcelo.console.routes
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.resources.Resource
+import io.ktor.server.application.call
+import io.ktor.server.resources.get
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+
+@Resource("/")
+class Health
+
+fun Route.healthRoutes() {
+    basicHealthRoute()
+}
+
+fun Route.basicHealthRoute() {
+    get<Health> {
+        call.respond(HttpStatusCode.OK)
+    }
+}
