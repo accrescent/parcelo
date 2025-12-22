3521636d04fd3630fdbffc7eceea72692c3b3abc
lberrymage <logan@lberrymage.dev>
2025-12-22 20:14:22 +0000

    Perform API tests as black-box tests
    Our approach of performing API tests as white-box tests in the way we
    have been is problematic for two main reasons:
    
    1. It is not as thorough as we need.
    2. It is unnecessarily complicated to implement.
    
    White-box tests are not thorough enough for our needs because they
    require us to import artificial database information in test setup which
    may not reflect what the application would actually create, causing us
    to test behavior that our application doesn't actually exhibit and thus
    rendering our tests ineffective. Furthermore, white-box tests are
    unnecessarily complicated to implement because, e.g., resetting the
    database between tests is made more difficult by the fact that the
    initial imported data needs to be retained between tests, a pattern that
    Quarkus does not easily support. Our current setup also requires
    developers to pass -Dquarkus.profile=test to quarkusIntTest, which may
    cause unexpected errors for new contributors.
    
    To solve these issues, this change removes all test dependencies on
    test-specific data, making our tests true black-box tests. It also adds
    a dedicated configuration profile for integration tests to separate
    their configuration from unit test configuration.
    
    Although not required by this change, all application-*.properties files
    were merged into a single config file to prevent unnecessary property
    duplication across them.

--- parcelo/build.gradle.kts
+++ parcelo/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation(libs.quarkus.kotlin)
     implementation(libs.quarkus.oidc)
     implementation(libs.quarkus.rest.jackson)
+    testImplementation(libs.htmlunit)
     testImplementation(libs.quarkus.junit5)
 }
 
