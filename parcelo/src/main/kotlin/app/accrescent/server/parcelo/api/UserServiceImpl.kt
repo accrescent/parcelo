96d5150a44e72d7a54872686cd2c6b901ae4f3ae
lberrymage <logan@lberrymage.dev>
2025-12-30 22:21:09 +0000

    Implement `UserService.GetSelf()`

---
+++ parcelo/src/main/kotlin/app/accrescent/server/parcelo/api/UserServiceImpl.kt
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: © 2025 Logan Magee
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package app.accrescent.server.parcelo.api
+
+import app.accrescent.appstore.publish.v1alpha1.GetSelfRequest
+import app.accrescent.appstore.publish.v1alpha1.GetSelfResponse
+import app.accrescent.appstore.publish.v1alpha1.UserService
+import app.accrescent.appstore.publish.v1alpha1.getSelfResponse
+import app.accrescent.server.parcelo.security.AuthnContextKey
+import app.accrescent.server.parcelo.security.GrpcAuthenticationInterceptor
+import app.accrescent.server.parcelo.validation.GrpcRequestValidationInterceptor
+import io.quarkus.grpc.GrpcService
+import io.quarkus.grpc.RegisterInterceptor
+import io.smallrye.mutiny.Uni
+import jakarta.transaction.Transactional
+
+@GrpcService
+@RegisterInterceptor(GrpcAuthenticationInterceptor::class)
+@RegisterInterceptor(GrpcRequestValidationInterceptor::class)
+class UserServiceImpl : UserService {
+    @Transactional
+    override fun getSelf(request: GetSelfRequest): Uni<GetSelfResponse> {
+        val userId = AuthnContextKey.USER_ID.get()
+
+        val response = getSelfResponse { this.userId = userId.toString() }
+
+        return Uni.createFrom().item { response }
+    }
+}
