5b69d35e7b7a9713431810b1acb4ae26c9574777
lberrymage <logan@lberrymage.dev>
2026-02-24 00:36:56 +0000

    Add more detailed tests for `AndroidManifest.parse()`

--- parcelo/src/test/kotlin/app/accrescent/server/parcelo/parsers/AndroidManifestTest.kt
+++ parcelo/src/test/kotlin/app/accrescent/server/parcelo/parsers/AndroidManifestTest.kt
@@ -4,32 +4,130 @@
 
 package app.accrescent.server.parcelo.parsers
 
+import arrow.core.left
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertNotNull
-
-private const val ANDROID_MANIFEST_WITH_NONCONTIGUOUS_PERMISSIONS_PATH =
-    "android-manifest-with-noncontiguous-permissions.xml"
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class AndroidManifestTest {
-    private val androidManifestWithNoncontiguousPermissions = javaClass
-        .classLoader
-        .getResourceAsStream(ANDROID_MANIFEST_WITH_NONCONTIGUOUS_PERMISSIONS_PATH)!!
-        .use { resourceStream -> resourceStream.bufferedReader().use { it.readText() } }
+    private val androidManifestWithNoncontiguousPermissions =
+        loadResource("android-manifest-with-noncontiguous-permissions.xml")
+    private val androidManifestWithoutDebuggable =
+        loadResource("android-manifest-without-debuggable.xml")
+    private val androidManifestWithoutTestOnly =
+        loadResource("android-manifest-without-test-only.xml")
+    private val androidManifestWithoutMinSdkVersion =
+        loadResource("android-manifest-without-min-sdk-version.xml")
+    private val androidManifestWithoutTargetSdkVersion =
+        loadResource("android-manifest-without-target-sdk-version.xml")
 
     @Test
     fun parseWithNoncontiguousPermissionsIncludesAllPermissions() {
-        val permissions = AndroidManifest
+        val manifest = AndroidManifest
             .parse(androidManifestWithNoncontiguousPermissions)
-            ?.usesPermissions
+            .getOrNull()!!
 
-        assertNotNull(permissions)
         assertEquals(
             listOf(
                 UsesPermission("android.permission.CAMERA", null),
                 UsesPermission("android.permission.ACCESS_COARSE_LOCATION", null),
             ),
-            permissions,
+            manifest.usesPermissions,
         )
     }
+
+    @ParameterizedTest
+    @MethodSource("genParamsForParseRejectsInvalidManifest")
+    fun parseRejectsInvalidManifest(params: ParseRejectsInvalidManifestParams) {
+        assertEquals(
+            params.expectedError.left(),
+            AndroidManifest.parse(loadResource(params.resourcePath)),
+        )
+    }
+
+    @Test
+    fun parseDefaultsDebuggableToFalse() {
+        val manifest = AndroidManifest
+            .parse(androidManifestWithoutDebuggable)
+            .getOrNull()!!
+
+        assertFalse(manifest.application.debuggable)
+    }
+
+    @Test
+    fun parseDefaultsTestOnlyToFalse() {
+        val manifest = AndroidManifest
+            .parse(androidManifestWithoutTestOnly)
+            .getOrNull()!!
+
+        assertFalse(manifest.application.testOnly)
+    }
+
+    @Test
+    fun parseDefaultsMinSdkVersionToOne() {
+        val manifest = AndroidManifest
+            .parse(androidManifestWithoutMinSdkVersion)
+            .getOrNull()!!
+
+        assertEquals(1, manifest.usesSdk?.minSdkVersion)
+    }
+
+    @Test
+    fun parseDefaultsTargetSdkVersionToMinSdkVersion() {
+        val manifest = AndroidManifest
+            .parse(androidManifestWithoutTargetSdkVersion)
+            .getOrNull()!!
+
+        assertEquals(33, manifest.usesSdk?.targetSdkVersion)
+    }
+
+    private fun loadResource(path: String): String {
+        return javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .use { resourceStream -> resourceStream.bufferedReader().use { it.readText() } }
+    }
+
+    private companion object {
+        @JvmStatic
+        private fun genParamsForParseRejectsInvalidManifest(): List<ParseRejectsInvalidManifestParams> {
+            return listOf(
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-with-too-long-app-id.xml",
+                    expectedError = AndroidManifestParseError.AppIdTooLong(128, 129),
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-with-invalid-app-id-format.xml",
+                    expectedError = AndroidManifestParseError.InvalidAppIdFormat,
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-with-too-small-version-code.xml",
+                    expectedError = AndroidManifestParseError.VersionCodeTooSmall(1, 0),
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-without-package.xml",
+                    expectedError = AndroidManifestParseError.MissingAppId,
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-without-version-code.xml",
+                    expectedError = AndroidManifestParseError.MissingVersionCode,
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-without-application.xml",
+                    expectedError = AndroidManifestParseError.ApplicationNotDefined,
+                ),
+                ParseRejectsInvalidManifestParams(
+                    resourcePath = "android-manifest-with-unnamed-permission.xml",
+                    expectedError = AndroidManifestParseError.PermissionNameNotDefined,
+                ),
+            )
+        }
+    }
 }
+
+data class ParseRejectsInvalidManifestParams(
+    val resourcePath: String,
+    val expectedError: AndroidManifestParseError,
+)
