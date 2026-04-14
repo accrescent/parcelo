ff920fbee9c987a7260bd26b3c333b73ec5f9755
lberrymage <logan@lberrymage.dev>
2026-04-14 02:05:20 +0000

    Remove gRPC request validator cache warming
    We don't clearly get a significant performance benefit from pre-warming
    the protovalidate descriptor cache, so it would be better to simplify
    our code by removing the cache warming setup. There are also reports
    that pre-warming the cache too much can cause significant memory
    consumption. Finally, it is too easy right now with the way our tests
    are defined to accidentally forget to add a new RPC request descriptor
    to the immutable cache, causing unexpected request validation errors.

--- parcelo/src/main/kotlin/app/accrescent/server/parcelo/security/GrpcRequestValidationInterceptor.kt
+++ parcelo/src/main/kotlin/app/accrescent/server/parcelo/security/GrpcRequestValidationInterceptor.kt
@@ -4,43 +4,6 @@
 
 package app.accrescent.server.parcelo.security
 
-import app.accrescent.appstore.v1.GetAppDownloadInfoRequest
-import app.accrescent.appstore.v1.GetAppListingRequest
-import app.accrescent.appstore.v1.GetAppPackageInfoRequest
-import app.accrescent.appstore.v1.GetAppUpdateInfoRequest
-import app.accrescent.console.v1.CreateAppDraftListingRequest
-import app.accrescent.console.v1.CreateAppDraftRequest
-import app.accrescent.console.v1.CreateAppDraftReviewRequest
-import app.accrescent.console.v1.CreateAppEditListingRequest
-import app.accrescent.console.v1.CreateAppEditRequest
-import app.accrescent.console.v1.CreateAppEditReviewRequest
-import app.accrescent.console.v1.DeleteAppDraftListingRequest
-import app.accrescent.console.v1.DeleteAppDraftRequest
-import app.accrescent.console.v1.DeleteAppEditListingRequest
-import app.accrescent.console.v1.DeleteAppEditRequest
-import app.accrescent.console.v1.DownloadAppDraftListingIconRequest
-import app.accrescent.console.v1.DownloadAppDraftRequest
-import app.accrescent.console.v1.DownloadAppEditRequest
-import app.accrescent.console.v1.GetAppDraftRequest
-import app.accrescent.console.v1.GetAppEditRequest
-import app.accrescent.console.v1.GetAppRequest
-import app.accrescent.console.v1.GetOrganizationRequest
-import app.accrescent.console.v1.GetSelfRequest
-import app.accrescent.console.v1.ListAppDraftsRequest
-import app.accrescent.console.v1.ListAppEditsRequest
-import app.accrescent.console.v1.ListAppsRequest
-import app.accrescent.console.v1.ListOrganizationsRequest
-import app.accrescent.console.v1.PublishAppDraftRequest
-import app.accrescent.console.v1.SubmitAppDraftRequest
-import app.accrescent.console.v1.SubmitAppEditRequest
-import app.accrescent.console.v1.UpdateAppDraftRequest
-import app.accrescent.console.v1.UpdateAppEditRequest
-import app.accrescent.console.v1.UpdateAppRequest
-import app.accrescent.console.v1.UpdateUserRequest
-import app.accrescent.console.v1.UploadAppDraftListingIconRequest
-import app.accrescent.console.v1.UploadAppDraftRequest
-import app.accrescent.console.v1.UploadAppEditListingIconRequest
-import app.accrescent.console.v1.UploadAppEditRequest
 import app.accrescent.server.parcelo.api.error.CommonApiError
 import app.accrescent.server.parcelo.api.error.CommonErrorReason
 import build.buf.protovalidate.Validator
@@ -53,57 +16,12 @@ import io.grpc.ServerCall
 import io.grpc.ServerCallHandler
 import io.grpc.ServerInterceptor
 import jakarta.enterprise.context.ApplicationScoped
-import app.accrescent.appstore.v1.ListAppListingsRequest as AppStoreListAppListingsRequest
-import app.accrescent.console.v1.ListAppListingsRequest as ConsoleListAppListingsRequest
 
 @ApplicationScoped
 class GrpcRequestValidationInterceptor : ServerInterceptor {
     private val validator = ValidatorFactory
         .newBuilder()
-        .buildWithDescriptors(
-            listOf(
-                AppStoreListAppListingsRequest.getDescriptor(),
-                ConsoleListAppListingsRequest.getDescriptor(),
-                CreateAppDraftListingRequest.getDescriptor(),
-                CreateAppDraftRequest.getDescriptor(),
-                CreateAppDraftReviewRequest.getDescriptor(),
-                CreateAppEditListingRequest.getDescriptor(),
-                CreateAppEditRequest.getDescriptor(),
-                CreateAppEditReviewRequest.getDescriptor(),
-                DeleteAppDraftListingRequest.getDescriptor(),
-                DeleteAppDraftRequest.getDescriptor(),
-                DeleteAppEditListingRequest.getDescriptor(),
-                DeleteAppEditRequest.getDescriptor(),
-                DownloadAppDraftRequest.getDescriptor(),
-                DownloadAppDraftListingIconRequest.getDescriptor(),
-                DownloadAppEditRequest.getDescriptor(),
-                GetAppDownloadInfoRequest.getDescriptor(),
-                GetAppDraftRequest.getDescriptor(),
-                GetAppEditRequest.getDescriptor(),
-                GetAppListingRequest.getDescriptor(),
-                GetAppPackageInfoRequest.getDescriptor(),
-                GetAppRequest.getDescriptor(),
-                GetAppUpdateInfoRequest.getDescriptor(),
-                GetOrganizationRequest.getDescriptor(),
-                GetSelfRequest.getDescriptor(),
-                ListAppDraftsRequest.getDescriptor(),
-                ListAppEditsRequest.getDescriptor(),
-                ListAppsRequest.getDescriptor(),
-                ListOrganizationsRequest.getDescriptor(),
-                PublishAppDraftRequest.getDescriptor(),
-                SubmitAppDraftRequest.getDescriptor(),
-                SubmitAppEditRequest.getDescriptor(),
-                UpdateAppDraftRequest.getDescriptor(),
-                UpdateAppEditRequest.getDescriptor(),
-                UpdateAppRequest.getDescriptor(),
-                UpdateUserRequest.getDescriptor(),
-                UploadAppDraftListingIconRequest.getDescriptor(),
-                UploadAppDraftRequest.getDescriptor(),
-                UploadAppEditListingIconRequest.getDescriptor(),
-                UploadAppEditRequest.getDescriptor(),
-            ),
-            true,
-        )
+        .build()
 
     override fun <ReqT : Any, RespT : Any> interceptCall(
         call: ServerCall<ReqT, RespT>,
