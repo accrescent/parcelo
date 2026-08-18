c9162c9368281b165622c6b0a87b2d313c299c7c
lberrymage <logan@lberrymage.dev>
2026-08-18 02:50:24 +0000

    Add Sha256Hash class
    This class will be used in various places which require a hash, e.g.,
    the upcoming session persistence API so that we don't accidentally
    attempt to save unhashed values.

---
+++ parcelo/src/main/kotlin/app/accrescent/server/parcelo/domain/crypto/Sha256Hash.kt
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: © 2026 Logan Magee
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package app.accrescent.server.parcelo.domain.crypto
+
+import app.accrescent.server.parcelo.core.Bytes
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
+import java.security.MessageDigest
+
+/**
+ * A SHA-256 hash over some data.
+ */
+@JvmInline
+value class Sha256Hash private constructor(private val digest: Bytes) {
+    companion object {
+        /**
+         * Creates a SHA-256 hash from an existing digest.
+         *
+         * @param digest the raw SHA-256 digest.
+         * @return a SHA-256 hash if the digest is exactly 32 bytes long, otherwise [None].
+         */
+        fun fromDigest(digest: ByteArray): Option<Sha256Hash> {
+            return if (digest.size == 32) {
+                Some(Sha256Hash(Bytes(digest)))
+            } else {
+                None
+            }
+        }
+
+        /**
+         * Hashes data to produce a SHA-256 hash.
+         *
+         * @param data the data to produce a SHA-256 hash for.
+         * @return the SHA-256 hash of [data].
+         */
+        fun hash(data: ByteArray): Sha256Hash {
+            return MessageDigest.getInstance("SHA-256").digest(data).let(::Bytes).let(::Sha256Hash)
+        }
+    }
+
+    /**
+     * Returns the raw digest represented by this hash.
+     *
+     * @return the raw digest represented by this hash value.
+     */
+    fun digest(): Bytes {
+        return digest
+    }
+}
